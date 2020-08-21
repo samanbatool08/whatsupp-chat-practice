6c0d1dc2f17f3a3ddc38099c7dc6fdf830eb52ab
Your Name <samanbatool08@gmail.com>
2020-08-20 23:19:00 -0400

    connected firebase db

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC8BTxYdl2r9X4YpGItCYtrH4RZbzU3tbc",
+    authDomain: "whatsupp-chat.firebaseapp.com",
+    databaseURL: "https://whatsupp-chat.firebaseio.com",
+    projectId: "whatsupp-chat",
+    storageBucket: "whatsupp-chat.appspot.com",
+    messagingSenderId: "986067042276",
+    appId: "1:986067042276:web:5aa2e6814094abc54011f1",
+    measurementId: "G-2C0FELFS98"
+  };
+
+  
