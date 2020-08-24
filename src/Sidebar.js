9898eac8cd359e3b626fb3fe84d4a0d7ddf821e3
Your Name <samanbatool08@gmail.com>
2020-08-24 17:44:23 -0400

    split screen

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Avatar } from '@material-ui/core';
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
 import './Sidebar.css'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className='sidebar__header'>
-            <Avatar />
+                <Avatar />
             <div className='sidebar__headerRight'>
+                <DonutLargeIcon />
+                <ChatIcon />
+                <MoreVertIcon />
 
             </div>
             </div>
