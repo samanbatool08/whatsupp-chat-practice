d047ae91760f78080b0266fca1808d2d13f197bf
Your Name <samanbatool08@gmail.com>
2020-08-24 19:23:55 -0400

    add new chat

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Avatar } from '@material-ui/core';
 import './SidebarChat.css'
 
-function SidebarChat() {
+function SidebarChat({ addNewChat }) {
     const [seed, setSeed] = useState('')
 
     useEffect(() => {
@@ -15,13 +15,26 @@ function SidebarChat() {
     //     setSeed(1234)
     // }, [])
     
-    return (
+    const createChat = () => {
+        const roomName = prompt('Please enter name for chat');
+
+        if(roomName){
+            // do some stuff here 
+        }
+    };
+
+    return !addNewChat ? (
         <div className='sidebarChat'>
             <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className='sidebarChat__info'>
                 <h2>Room name</h2>
                 <p>Last message...</p>
             </div>
+        </div>
+    ) : (
+        <div onClick={createChat}
+            className='sidebar__chat'>
+                <h2>Add new chat</h2>
         </div>
     )
 }
