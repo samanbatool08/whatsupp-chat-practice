7216778deb517bbb2aaca07c8457f9f34821a226
Your Name <samanbatool08@gmail.com>
2020-08-24 19:03:22 -0400

    randomize seeds using useeffect

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar } from '@material-ui/core';
 import './SidebarChat.css'
 
 function SidebarChat() {
+    const [seed, setSeed] = useState('')
+
+    useEffect(() => {
+        setSeed(Math.floor(
+            Math.random() * 5000
+        ))
+    }, [])
+
     return (
         <div className='sidebarChat'>
-            <Avatar src='https://avatars.dicebear.com/api/human/123.svg'/>
+            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
             <div className='sidebarChat__info'>
                 <h2>Room name</h2>
                 <p>Last message...</p>
