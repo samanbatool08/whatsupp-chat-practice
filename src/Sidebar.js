2514616112c24485bcd8370b2442731ae26a9c6f
Your Name <samanbatool08@gmail.com>
2020-08-24 18:03:40 -0400

    input search

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Avatar } from '@material-ui/core';
+import { Avatar, IconButton } from '@material-ui/core';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import  { SearchOutlined } from '@material-ui/icons';
 
 import './Sidebar.css'
 
@@ -12,14 +13,26 @@ function Sidebar() {
             <div className='sidebar__header'>
                 <Avatar />
             <div className='sidebar__headerRight'>
-                <DonutLargeIcon />
-                <ChatIcon />
-                <MoreVertIcon />
+                <IconButton>
+                    <DonutLargeIcon />
+                </IconButton>
+
+                <IconButton>
+                    <ChatIcon />
+                </IconButton>
+
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
 
             </div>
             </div>
 
             <div className='sidebar__search'>
+                <div className='sidebar__searchContainer'>
+                    <SearchOutlined />
+                    <input placeholder='Search or start a new chat' type='text' />
+                </div>
 
 
             </div>
