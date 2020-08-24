e13eefc85132dcc05be44fbc07f89d66d03f8b58
Your Name <samanbatool08@gmail.com>
2020-08-24 17:26:36 -0400

    avatar icon

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Avatar } from '@material-ui/core';
 import './Sidebar.css'
 
 function Sidebar() {
     return (
         <div className='sidebar'>
             <div className='sidebar__header'>
+            <Avatar />
+            <div className='sidebar__headerRight'>
 
-
+            </div>
             </div>
 
             <div className='sidebar__search'>
