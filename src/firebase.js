5de26ad0aa86039d5b7e8c0d12d99ed28b3c28ac
Your Name <samanbatool08@gmail.com>
2020-09-03 20:35:49 -0400

    authentication handler

--- src/firebase.js
+++ src/firebase.js
@@ -14,4 +14,5 @@ const firebaseConfig = {
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
   const auth = firebase.auth();
-  
+
+  const provider = new firebase.auth.GoogleAuthProvider();
