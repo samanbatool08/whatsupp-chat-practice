23f255f3605456ea23a0a9f4d153ae1441c9806d
Your Name <samanbatool08@gmail.com>
2020-09-13 22:28:57 -0400

    router working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar'
-import Chat from './Chat'
+import Chat from './Chat';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
 
      <div className='app__body'>
-       {/* Sidebar */}
-       <Sidebar />
-       <Chat />
+       <Router>
+         <Switch>
+           <Route path='/'>
+             <h1>hitting home screen</h1>
+           </Route>
+          <Sidebar />
+           <Chat />
+        </Switch>
 
+
+       </Router>
      </div>
     </div>
   );
