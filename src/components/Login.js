fb3540baee9dc2426dd5bcaab3547ebd9b64fb4f
Your Name <samanbatool08@gmail.com>
2020-10-02 12:38:04 -0400

    update login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -32,7 +32,7 @@ function Login() {
                     alt=''
                     />
                 <div className='login__text'>
-                    <h1>Sign in to WhatsApp</h1>
+                    <h1>Sign in to WhatsUpp</h1>
                 </div>
 
                 <Button type='submit' onClick={signIn}>
